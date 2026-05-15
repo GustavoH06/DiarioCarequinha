bf7293084ac93b31ab8ab69d46a45a44f6740a1b
GustavoH06 <gustavocaixeta7@hotmail.com>
2026-05-15 14:14:49 -0300

    front listas testes

--- gerenciadorCarequinha/gerenciador-carequinha/src/pages/AlunoList.jsx
+++ gerenciadorCarequinha/gerenciador-carequinha/src/pages/AlunoList.jsx
@@ -1,5 +1,41 @@
 function AlunoList(){
-    return'aluno list';
+    return(
+        <div className="list-container">
+            <h1>Lista de Salas</h1>
+            <div className="filter-container">
+                <div className="filter-input">
+                    <input type="text" name="name" placeholder="Nome"/>
+                    <input type="text" name="age" placeholder="Tipo Turma"/>
+                    <input type="text" name="job" placeholder="Id"/>
+                    <input type="text" name="a" placeholder="Turno"/>
+                </div>
+                <div className="filter-buttons">
+                    <i class="bi bi-trash"></i>
+                    <i class="bi bi-search" style={{}}></i>
+                </div>
+            </div>
+
+            <div className="salas-list">
+                <div className="list-container">
+                    <div className="item-list header">
+                        <label>Id</label>
+                        <label>Nome</label>
+                        <label>Tipo</label>
+                        <label>Turno</label>
+                        <label>Alunos</label>
+                    </div>
+
+                    <div className="item-list content">
+                        <label>1</label>
+                        <label>Berçario 1</label>
+                        <label>Berçario</label>
+                        <label>Manhã</label>
+                        <label>17</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default AlunoList;
--- gerenciadorCarequinha/gerenciador-carequinha/src/pages/SalaList.jsx
+++ gerenciadorCarequinha/gerenciador-carequinha/src/pages/SalaList.jsx
@@ -1,5 +1,41 @@
 function SalaList(){
-    return'sala list';
+    return(
+        <div className="list-container">
+            <h1>Lista de Alunos</h1>
+            <div className="filter-container">
+                <div className="filter-input">
+                    <input type="text" name="name" placeholder="Nome"/>
+                    <input type="text" name="age" placeholder="Turno"/>
+                    <input type="text" name="job" placeholder="Id"/>
+                    <input type="text" name="a" placeholder="Sala"/>
+                </div>
+                <div className="filter-buttons">
+                    <i class="bi bi-trash"></i>
+                    <i class="bi bi-search" style={{}}></i>
+                </div>
+            </div>
+
+            <div className="salas-list">
+                <div className="list-container">
+                    <div className="item-list header">
+                        <label>Id</label>
+                        <label>Nome</label>
+                        <label>Sala</label>
+                        <label>Sexo</label>
+                        <label>Idade</label>
+                    </div>
+
+                    <div className="item-list content">
+                        <label>1</label>
+                        <label>Kaique Fernandes</label>
+                        <label>Berçario 1</label>
+                        <label>M</label>
+                        <label>6</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default SalaList;
